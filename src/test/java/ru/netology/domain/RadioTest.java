b4010e1b6d66dd75b2635d13d60a83ef72b232dd
Brejnev1917 <Brejnev1917@gmail.com>
2021-12-02 12:39:39 +0300

    add get&set and test

---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,72 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    void getMaxVolume() {
+    }
+
+    @Test
+    void setMaxVolume() {
+    }
+
+    @Test
+    void getMinVolume() {
+    }
+
+    @Test
+    void setMinVolume() {
+    }
+
+    @Test
+    void getCurrentVolume() {
+    }
+
+    @Test
+    void setCurrentVolume() {
+    }
+
+    @Test
+    void upVolume() {
+    }
+
+    @Test
+    void downVolume() {
+    }
+
+    @Test
+    void getMaxStation() {
+    }
+
+    @Test
+    void setMaxStation() {
+    }
+
+    @Test
+    void getMinStation() {
+    }
+
+    @Test
+    void setMinStation() {
+    }
+
+    @Test
+    void getCurrentStation() {
+    }
+
+    @Test
+    void upStation() {
+    }
+
+    @Test
+    void downStation() {
+    }
+
+    @Test
+    void setCurrentStation() {
+    }
+}

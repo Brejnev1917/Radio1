66802a0ed2ce1027c30cfee54bf976f5ab06c51c
Brejnev1917 <Brejnev1917@gmail.com>
2021-12-02 12:18:26 +0300

    Initial commit

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,13 @@
+package ru.netology.domain;
+
+public class Radio {
+    String name;
+        private int maxVolume;
+        private int minVolume;
+        private int currentVolume;
+        private int maxStation;
+        private int minStation;
+        private int currentStation;
+    }
+
+
